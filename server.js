037f8a9fa525c7ae88de1e544a951f84268dd765
yoad-hordan4 <your-email@example.com>
2025-03-01 09:46:09 +0200

    secured my ID

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const axios = require("axios");
-const cors = require("cors");
-require("dotenv").config();
+const express = require("express");//express → A framework to create a web server in Node.js.
+const axios = require("axios");//axios → A library for making HTTP requests (fetching data from the internet).
+const cors = require("cors"); // cors → Stands for Cross-Origin Resource Sharing, allowing your backend to accept requests from different origins (e.g., frontend hosted elsewhere).
+require("dotenv").config(); //dotenv → A package that lets us store sensitive information (like API keys) in a .env file instead of hardcoding them in our code.
+
 
 const app = express();
 app.use(cors());
 
-const CLIENT_ID = "783715a9cebc4d2ea192a544652bd00c";
-const CLIENT_SECRET = "9dbbd3a11e1d46c28bf5b7d19d46d1b1";
+const CLIENT_ID = "783715a9cebc4d2ea192a544652bd00c";  
+const CLIENT_SECRET = "9dbbd3a11e1d46c28bf5b7d19d46d1b1"; 
 const REDIRECT_URI = "http://localhost:3000/callback";
 
 app.get("/login", (req, res) => {
